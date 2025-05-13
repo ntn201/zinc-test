533aba772654e349d48e2d495f25cd58a5596488
NTN <nguyen.nt@baohiemtasco.vn>
2025-05-13 23:16:27 +0700

    refactor migration flow

--- migrations/env.py
+++ migrations/env.py
@@ -5,35 +5,21 @@ from sqlalchemy import pool
 
 from alembic import context
 
-# import os
-# import sys
-# from src.utils.db import engine as target_metadata_engine
+
+
+import os
 from src.models import *
 from sqlmodel import SQLModel
 
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = SQLModel.metadata
-print(target_metadata)
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+sqlalchemy_url = os.getenv("MIGRATION_DATABASE_URL")
+config.set_main_option("sqlalchemy.url", sqlalchemy_url)
 
 
 def run_migrations_offline() -> None:
